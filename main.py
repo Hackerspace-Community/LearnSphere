5bfa538aeff21c0165fa1f14a2750c99c4e4a6f3
Harshit Kr Vishwakarma <vharshitkr01gmail.com>
2023-05-20 14:34:10 +0530

    form submit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
@@ -7,6 +7,15 @@ Bootstrap(app)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    # get form data
+    data = request.get_json()
+        
+        # Print the received data
+    print(data)
+    return render_template('submit.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000,debug=True)
