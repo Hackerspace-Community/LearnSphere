4e84bb3f1e4c21328d4a16eaeacf79bf07c9d7be
Harshit Kr Vishwakarma <vharshitkr01gmail.com>
2023-05-20 20:48:49 +0530

    ui update

--- gpt.py
+++ gpt.py
@@ -39,8 +39,25 @@ def query(resdata):
     # 6 -7 days in a week and 2-4 hrs a day and i can learn from video audio and
     #  book and also say how much time it will take along with links
 
-    query = f"Give me a learning path for {resdata['subject']} in {resdata['field']} i know {knowlst} i can give {resdata['days']} in a week and {'hrs'} per day i prefer learning from {learnlst} also tell me how much time it will take to complete and provide the links too."
-    # query2 = "what is 2+2"
+    query = f'''
+            Provide a learning path for {resdata['subject']} in 
+            {resdata['field']}. I have prior knowledge of {knowlst} 
+            and can dedicate {resdata['days']} days per week and {'hrs'} 
+            hours per day to learning. I prefer learning from {learnlst}. 
+            Please include the estimated time to complete the learning 
+            path and provide links to the necessary resources.
+            
+
+            Additional Requirements:
+
+            1. Format your response using Bootstrap cards for a visually appealing layout.
+            2. Ensure that all links have the attribute target="_blank" to open in a new tab.
+            
+            Please provide your response as HTML code that adheres to these requirements, 
+            without the <head> and <body> tags. Avoid providing explanations.
+            '''
+
+    query2 = "what is 2+2"
     response = query_chatgpt(query)
     print(response)
     return response
